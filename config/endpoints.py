642d2b7ca76e5e957d5b9a71116e443d3be2337f
sdet0mis <sdet0mis@mail.ru>
2025-04-28 18:55:22 +0300

    test

--- config/endpoints.py
+++ config/endpoints.py
@@ -4,19 +4,19 @@ base_url = "https://petstore.swagger.io/v2/"
 class PetEndpoints:
 
     add_pet = f"{base_url}pet/"
-    find_pet = lambda self, pet_id: f"{base_url}pet/{pet_id}/"
-    delete_pet = lambda self, pet_id: f"{base_url}pet/{pet_id}/"
+    find_pet = lambda self, pet_id: f"{base_url}pet/{pet_id}/"  # noqa
+    delete_pet = lambda self, pet_id: f"{base_url}pet/{pet_id}/" # noqa
 
 
 class StoreEndpoints:
 
     add_order = f"{base_url}store/order"
-    find_order = lambda self, order_id: f"{base_url}store/order/{order_id}/"
-    delete_order = lambda self, order_id: f"{base_url}store/order/{order_id}/"
+    find_order = lambda self, order_id: f"{base_url}store/order/{order_id}/"  # noqa
+    delete_order = lambda self, order_id: f"{base_url}store/order/{order_id}/"  # noqa
 
 
 class UserEndpoints:
 
     create_user = f"{base_url}user/"
-    get_user = lambda self, user_name: f"{base_url}user/{user_name}/"
-    delete_user = lambda self, user_name: f"{base_url}user/{user_name}/"
+    get_user = lambda self, user_name: f"{base_url}user/{user_name}/"  # noqa
+    delete_user = lambda self, user_name: f"{base_url}user/{user_name}/"  # noqa
